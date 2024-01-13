4be3c3e55bdca36908f4622f6008620e6575c6c4
ChengZixuanChina <tom_114514@qq.com>
2024-01-13 13:29:11 +0800

    finish homework 乒乓球赛!

--- 2024.1.13.乒乓球赛.cpp
+++ 2024.1.13.乒乓球赛.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int n, a[101][101];
+    int n, a[101][101], score[101];
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
@@ -13,6 +13,23 @@ int main()
             cin >> a[i][j];
         }
     }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            score[i] += a[i][j];
+        }
+    }
+    int max = -1, max_id = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (score[i] > max)
+        {
+            max = score[i];
+            max_id = i;
+        }
+    }
+    cout << max_id << endl;
 
     system("pause");
     return 0;
